4beb93b59bec574866eca8b03b5e10ec4243fa24
Kheo-app <ch.stebler@gmail.com>
2020-02-13 13:01:57 +0100

    Implemented image defaults for Portfolio Items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,10 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_imgage, :thumb_image
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_imgage ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+  end
 end
